41abae8d954d19296dccb0594502ef84dd571089
Nave Adi <61701327+Terrfex@users.noreply.github.com>
2022-03-09 23:11:14 +0200

    Add files via upload

---
+++ EzoomArdu.ino
@@ -0,0 +1,35 @@
+#include<Servo.h>
+
+Servo serX;
+Servo serY;
+
+String serialData;
+
+void setup() {
+
+  serX.attach(11);
+  serY.attach(10);
+  Serial.begin(9600);//default broad rate of serial communication on Arduino
+  Serial.setTimeout(10);//10 ms delay(default is 1 sec) 
+}
+
+void loop() {
+  //lol
+}
+
+void serialEvent() {
+  serialData = Serial.readString();
+  serX.write(parseDataX(serialData));
+  serY.write(parseDataY(serialData));
+}
+
+int parseDataX(String data){
+  data.remove(data.indexOf("Y"));
+  data.remove(data.indexOf("X"), 1);
+  return data.toInt();
+}
+
+int parseDataY(String data){
+  data.remove(0,data.indexOf("Y") + 1);
+  return data.toInt();
+}
