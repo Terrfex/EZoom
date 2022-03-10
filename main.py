268d449b9d8db54a0bca2adac8b98ab072de9ab8
Nave Adi <61701327+Terrfex@users.noreply.github.com>
2022-03-10 09:16:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,76 @@
-String serialData;
+import cv2
+import math
 
-void setup() {
 
-  serX.attach(11);
-  serY.attach(10);
-  Serial.begin(9600);//default broad rate of serial communication on Arduino
-  Serial.setTimeout(10);//10 ms delay(default is 1 sec) 
-}
+def is_right(d_x, p_x):
+    if p_x < d_x:
+        return 1
+    return 0
 
-void loop() {
-  //lol
-}
 
-void serialEvent() {
-  serialData = Serial.readString();
-  serX.write(parseDataX(serialData));
-  serY.write(parseDataY(serialData));
-}
+def is_up(d_y, p_y):
+    if p_y < d_y:
+        return 1
+    return 0
 
-int parseDataX(String data){
-  data.remove(data.indexOf("Y"));
-  data.remove(data.indexOf("X"), 1);
-  return data.toInt();
-}
 
-int parseDataY(String data){
-  data.remove(0,data.indexOf("Y") + 1);
-  return data.toInt();
-}
+def dist_and_direction(d_x, d_y, p_x, p_y):
+    distan = math.sqrt((d_x - p_x) ** 2 + (d_y - p_y) ** 2)
+    up = is_up(d_y, p_y)
+    right = is_right(d_x, p_x)
 
+    return distan, right, up
+
+
+# returning the distance, isright(0\1), isup(0\1)
+
+
+def main():
+    faceCascade = cv2.CascadeClassifier('C:\\Users\\BigSale2u\\Desktop\\py\\haarcascade_frontalface_default.xml')
+
+    video_capture = cv2.VideoCapture(0)
+    p_x = 0
+    p_y = 0
+    d_x = 7
+    d_y = 7
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+
+        gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray_img,
+            scaleFactor=1.1,
+            minNeighbors=6,
+            minSize=(31, 31),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+        # choosing the biggest face available
+        if len(faces) >= 1:
+            face = faces[0]
+            for (x, y, w, h) in faces:
+                if w * h > face[2] * face[3]:
+                    face = (x, y, w, h)
+
+            # Draw a dot in the middle of the face
+            p_x = d_x
+            p_y = d_y
+            d_x = (face[0] + face[2]/2)
+            d_y = (face[1] + face[3]/2)
+            cv2.circle(frame, (int(d_x), int(d_y)), 10, (0, 255, 0), -1)  # unknown why there is a 300px offset
+            print("distance is: " + str(dist_and_direction(d_x, d_y, p_x, p_y)))
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything is done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
